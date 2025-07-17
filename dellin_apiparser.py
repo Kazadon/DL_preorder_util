88ec9ef05652fc910ff13e65ba17c9131bf1aef3
Kazadon <replaykazadon@gmail.com>
2025-07-17 19:51:49 +0300

    Major update

--- dellin_apiparser.py
+++ dellin_apiparser.py
@@ -3,26 +3,72 @@ import json
 from dotenv import dotenv_values
 # Переписать в ООП, добавить методы - авторизация, проверка сессии, вывод списка заказов на определенную дату от Гермеон, печать предварительных заявок
 
+
+class DellinScraper:
+
+    def __init__(self, token: str, login: str, password: str):
+        self.headers = {'accept': 'application/json',
+                        'Content-Type': 'application/json'}
+        self.login = login
+        self.password = password
+        self.token = token
+        self.sessionID = None
+    
+    # Аутентификация, получение sessionID для дальнейшей работы
+    def auth(self) -> None:
+        url = 'https://api.dellin.ru/v3/auth/login.json'
+        data = {"appkey": self.token,
+                "login": self.login,
+                "password": self.password}
+        json_string = json.dumps(data)
+        response = requests.post(url, headers=self.headers, data=json_string)
+        if response.status_code == 200:
+            print(f"\nResponse 200.\nAUTH - succesful request\n{response.text}")
+            self.sessionID = json.loads(response.content)['data']['sessionID']
+
+        else:
+            print("\nAUTH ERROR")
+            print(response)
+
+    # Проверка активности сессии
+    def check_session(self) -> None:
+        url = 'https://api.dellin.ru/v3/auth/session_info.json'
+        data = {"appKey": self.token,
+                "sessionID": self.sessionID}
+        json_string = json.dumps(data)
+        response = requests.post(url, headers=self.headers, data=json_string)
+
+        if response.status_code == 200:
+            print(f"\nResponse 200.\nCheck session - succesful request.\nsessionID: {data['sessionID']}\n{response.text}")
+        else:
+            print("\nCHECK SESSION ERROR \nWrong ApiToken/sessionID or something went wrong\nTry again to auth")
+            print(response)
+
+    def close_session(self) -> None:
+        url = 'https://api.dellin.ru/v3/auth/logout.json'
+        data = {"appKey": self.token,
+                "sessionID": self.sessionID}
+        json_string = json.dumps(data)
+        response = requests.post(url, headers=self.headers, data=json_string)
+
+        if response.status_code == 200:
+            print(f"\nResponse 200.\nLOGOUT - succesful request.\nSESSION CLOSED\n{response.text}")
+        else:
+            print("\nCLOSE SESSION ERROR\nWrong ApiToken/sessionID or something went wrong\nTry again")
+            print(response)
+
+    def order_list(self) -> None:
+        # 'https://api.dellin.ru/v3/orders.json'
+        pass
+
+    def documents_list(self) -> None:
+        # 'https://api.dellin.ru/v1/customers/request/pdf.json'
+        pass
+
 config = dotenv_values('.env')
-url = 'https://api.dellin.ru/v3/auth/session_info.json'
-
-headers = {'accept': 'application/json',
-           'Content-Type': 'application/json'
-}
+app = DellinScraper(config['DL_API_TOKEN'], config['LOGIN'], config['PASSWORD'])
+app.auth()
+app.check_session()
 
 
-data = {"appKey": config['DL_API_TOKEN'],
-        "sessionID":"A1975D78-0A48-4D88-8477-DC18999CAB17"
-        # "requestID":"55357415"
-        }
-json_data = json.dumps(data)
-response = requests.post(url, headers=headers, data=json_data)
-
-if response.status_code == 200:
-    print("Авторизация успешна")
-    print(response.text)
-
-else:
-    print("Ошибка авторизации")
-    print(response)
-    exit()
+app.close_session()
